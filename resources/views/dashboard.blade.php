8b68d7b183ec7e58fd222049c9f316e582b6ce18
Barreda Pinto <ZBarreda-tecsup>
2024-06-02 20:49:08 -0500

    git commit -g ""

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,7 @@
+@php
+$geolocationController = new App\Http\Controllers\GeolocationController();
+   $country = $geolocationController->getUserCountry();
+@endphp
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
