9bc47f1e5b71d468ff700bb3a9abe29c7d2ca96e
Barreda Pinto <ZBarreda-tecsup>
2024-06-02 21:24:27 -0500

    git commit -g ""

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/dashboard', [GeolocationController::class, 'getGeolocationData']);
 require __DIR__.'/auth.php';
 
